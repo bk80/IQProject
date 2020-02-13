0aa1e1330e3a1d12931be9c4a7f0ae9df6c3d822
bk80 <bimaldpatel80@gmail.com>
2020-02-12 23:06:52 -0500

    Add files via upload

---
+++ Holiday/CustomerController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Data;
+
+namespace WebApplication2.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var repo = new CustomersRepository();
+            var customerList = repo.GetCustomers();
+            return View(customerList);
+        }
+
+        //[HttpGet]
+        //public ActionResult GetRegions(string iso3)
+        //{
+        //    if (!string.IsNullOrWhiteSpace(iso3) && iso3.Length == 3)
+        //    {
+        //        var repo = new RegionsRepository();
+
+        //        IEnumerable<SelectListItem> regions = repo.GetRegions(iso3);
+        //        return Json(regions);
+        //    }
+        //    return null;
+        //}
+
+        [HttpGet]
+        public ActionResult GetHoliday(string year)
+        {
+            if (!string.IsNullOrWhiteSpace(year))
+            {
+                var repo = new HolidayRepository();
+
+                var regions = repo.GetHolidays(year);
+                return Json(regions);
+            }
+            return null;
+        }
+
+        //// GET: Customer/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        //// GET: Customer/Create
+        //public ActionResult Create()
+        //{
+        //    var repo = new CustomersRepository();
+        //    var customer = repo.CreateCustomer();
+        //    return View(customer);
+        //}
+
+       
+    }
+}
+
---
+++ Holiday/HolidayRepository.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Model;
+
+namespace WebApplication2.Data
+{
+    public class HolidayRepository
+    {
+        public IEnumerable<SelectListItem> GetHolidays()
+        {         
+                    var holidays = new List<HolidayCalendar>();
+                    for (int i = 0; i < 50; i++)
+                    {
+                        holidays.Add(new HolidayCalendar()
+                          {
+                            Id=i,
+                            HolidayDate = DateTime.Now.AddDays(new Random().Next(1, 3000))
+                           }
+                        );
+                    }
+           
+                    var holidayItems = holidays
+                        .OrderBy(n => n.HolidayDate)
+                        .GroupBy(n=>n.HolidayDate.Year)
+                        .Select(n =>
+                           new SelectListItem
+                           {
+                               Value = n.FirstOrDefault()?.HolidayDate.Year.ToString(),
+                               Text = n.FirstOrDefault()?.HolidayDate.Year.ToString()
+                           }).ToList();
+
+                        var countrytip = new SelectListItem()
+                        {
+                            Value = null,
+                            Text = "--- select Holiday ---"
+                        };
+                     holidayItems.Insert(0, countrytip);
+            return new SelectList(holidayItems, "Value", "Text");
+               
+        }
+
+        public IEnumerable<HolidayCalendar> GetHolidays(string year)
+        {
+            if (!String.IsNullOrWhiteSpace(year))
+            {
+                var selectedYear = Convert.ToInt32(year);
+               
+                var holidays = new List<HolidayCalendar>();
+                for (int i = 0; i < 50; i++)
+                {
+                    holidays.Add(new HolidayCalendar()
+                    {
+                        Id = i,
+                        HolidayDate = DateTime.Now.AddDays(new Random().Next(1, 3000))
+                    }
+                    );
+                }
+
+                //using (var context = new ApplicationDbContext())
+                {
+                    var HolidayYears = holidays
+                         .OrderBy(n => n.HolidayDate)
+                         .Where(n => n.HolidayDate.Year == selectedYear)
+                         .Select(n=>
+                            new HolidayCalendar {
+                                Id=n.Id,
+                                HolidayDate=n.HolidayDate,
+                                DisplayDate=n.HolidayDate.ToShortDateString()
+                            });
+
+                    return HolidayYears;
+                }
+            }
+            return null;
+        }
+    }
+}
